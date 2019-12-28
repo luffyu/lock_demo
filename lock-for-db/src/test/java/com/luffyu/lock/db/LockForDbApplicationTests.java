ab1bbce53e78dc7f12047b84a846b8b253879e4e
yugaofeng <Ygf@33520>
2019-12-28 16:09:05 +0800

    新建lock-for-db项目

---
+++ lock-for-db/src/test/java/com/luffyu/lock/db/LockForDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luffyu.lock.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockForDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
