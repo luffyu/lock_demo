d7a7c9ec4470bd133044aada1f04da08e7ac460e
yugaofeng <Ygf@33520>
2019-12-30 11:19:05 +0800

    zookeeper添加watch方法

--- lock_for_zookeeper/src/main/java/com/luffyu/lock/zookeeper/service/ZookeeperLockService.java
+++ lock_for_zookeeper/src/main/java/com/luffyu/lock/zookeeper/service/ZookeeperLockService.java
@@ -2,6 +2,8 @@ package com.luffyu.lock.zookeeper.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.recipes.cache.PathChildrenCache;
+import org.apache.curator.framework.recipes.cache.PathChildrenCacheEvent;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.ZooDefs;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +22,7 @@ import java.util.concurrent.CountDownLatch;
 @Service
 public class ZookeeperLockService {
 
-    private final static String ROOT_PATH_LOCK = "/rootlock/";
+    private final static String ROOT_PATH_LOCK = "/rootLock";
 
     @Resource
     private CuratorFramework curatorFramework;
@@ -31,7 +33,7 @@ public class ZookeeperLockService {
      * 获取分布式锁
      */
     public boolean acquireLock(String path) {
-        String keyPath =  ROOT_PATH_LOCK + path;
+        String keyPath =  ROOT_PATH_LOCK + "/" +path;
         try {
             curatorFramework
                     .create()
@@ -53,7 +55,7 @@ public class ZookeeperLockService {
      */
     public boolean releaseLock(String path) {
         try {
-            String keyPath =  ROOT_PATH_LOCK +  path;
+            String keyPath =  ROOT_PATH_LOCK + "/" + path;
             if (curatorFramework.checkExists().forPath(keyPath) != null) {
                 curatorFramework.delete().forPath(keyPath);
             }
@@ -64,4 +66,47 @@ public class ZookeeperLockService {
         return true;
     }
 
+
+    /**
+     * 创建 watcher 事件
+     */
+    private void addWatcher(String path) throws Exception {
+        String keyPath;
+        if (path.equals(ROOT_PATH_LOCK)) {
+            keyPath = path;
+        } else {
+            keyPath =  ROOT_PATH_LOCK + "/" + path;
+        }
+        final PathChildrenCache cache = new PathChildrenCache(curatorFramework, keyPath, false);
+        cache.start(PathChildrenCache.StartMode.POST_INITIALIZED_EVENT);
+        cache.getListenable().addListener((client, event) -> {
+            if (event.getType().equals(PathChildrenCacheEvent.Type.CHILD_REMOVED)) {
+                String oldPath = event.getData().getPath();
+                log.info("上一个节点 "+ oldPath + " 已经被断开");
+                if (oldPath.contains(path)) {
+                    //释放计数器，让当前的请求获取锁
+                    //countDownLatch.countDown();
+                }
+            }
+        });
+    }
+
+    //创建父节点，并创建永久节点˚
+    public void afterPropertiesSet() {
+        curatorFramework = curatorFramework.usingNamespace("lock-namespace");
+        String path = ROOT_PATH_LOCK;
+        try {
+            if (curatorFramework.checkExists().forPath(path) == null) {
+                curatorFramework.create()
+                        .creatingParentsIfNeeded()
+                        .withMode(CreateMode.PERSISTENT)
+                        .withACL(ZooDefs.Ids.OPEN_ACL_UNSAFE)
+                        .forPath(path);
+            }
+            addWatcher(ROOT_PATH_LOCK);
+            log.info("root path 的 watcher 事件创建成功");
+        } catch (Exception e) {
+            log.error("connect zookeeper fail，please check the log >> {}", e.getMessage(), e);
+        }
+    }
 }
